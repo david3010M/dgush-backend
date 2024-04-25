765a1b5cd3f3382c25a9c2ad023fc9992bcb1546
david3010M <hvaldiviezos@unprg.edu.pe>
2024-04-25 00:05:38 -0500

    Permission Model and Controller

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @OA\Schema(
+ *     schema="Permission",
+ *     type="object",
+ *     @OA\Property(property="id", type="integer", example="1"),
+ *     @OA\Property(property="name", type="string", example="permission"),
+ *     @OA\Property(property="route", type="string", example="permission"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2021-09-01T00:00:00"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", example="2021-09-01T00:00:00"),
+ *     @OA\Property(property="deleted_at", type="string", format="date-time", example="2021-09-01T00:00:00"),
+ * )
+ *
+ */
 class Permission extends Model
 {
     use HasFactory;
--- database/migrations/2024_04_17_222939_create_permissions_table.php
+++ database/migrations/2024_04_17_222939_create_permissions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('name')->unique();
             $table->string('route')->unique();
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
