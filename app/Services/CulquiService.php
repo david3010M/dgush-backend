975b5f8c52dedd10d5f33de8f2783f0e3278b057
MiguelGuevaraC <mguevaracaj@unprg.edu.pe>
2025-05-24 11:32:23 -0500

    fix: se cambia el uuid demo a tienda en el online-store de 360

--- app/Services/CulquiService.php
+++ app/Services/CulquiService.php
@@ -59,7 +59,7 @@ class CulquiService
     ) {
         try {
             $url = "https://sistema.360sys.com.pe/api/online-store/" . $endpoint;
-            $authorizationUiid = !empty($authorizationUiid) ? $authorizationUiid : env('APP_UUID_DEMO_360');
+            $authorizationUiid = !empty($authorizationUiid) ? $authorizationUiid : env('APP_UUID_TIENDA');
 
             $response = HttP::withHeaders([
                 'Authorization' => $authorizationUiid,
